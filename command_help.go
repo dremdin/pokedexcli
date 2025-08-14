398997226eee7cb63a056363293433fdad219db6
Dremdin <dremdin@proton.me>
2025-08-14 12:21:27 +0200

    Added all commands for the lesson.

--- command_help.go
+++ command_help.go
@@ -1,15 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func commandHelp(cfg *config) error {
+func commandHelp(cfg *config, args ...string) error {
+	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
 	for _, cmd := range getCommands() {
-		fmt.Printf("\t%s: %s\n", cmd.name, cmd.description)
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
 	fmt.Println()
 	return nil
